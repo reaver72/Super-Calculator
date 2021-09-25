5d33237f9b2699df5957089c0bef68e4175bef4a
rawbeen72 <75427075+rawbeen72@users.noreply.github.com>
2021-09-25 05:48:06 -0700

    Update SuperCalculator.py

--- SuperCalculator.py
+++ SuperCalculator.py
@@ -369,11 +369,10 @@ class MyForm(QDialog):
         query = query.replace("+", "%2B")
         api_address = f"http://api.wolframalpha.com/v2/query?appid=Y98QH3-24PWX83VGA&input={query}&podstate=Result__Step-by-step+solution&format=plaintext&output=json"
         json_data = requests.get(api_address).json()
-        print(json_data)
+        
         try:
             try:
                 answer = json_data["queryresult"]["pods"][0]["subpods"][0]["plaintext"]
-                print(answer)
                 answer = answer.replace("sqrt", "√")
                 text = self.ui.lineEditDisplay.toPlainText()
                 self.ui.lineEditDisplay.setText(text+answer)
@@ -382,7 +381,6 @@ class MyForm(QDialog):
             try:
                 answer = json_data["queryresult"]["pods"][1]["subpods"][0]["plaintext"]
                 answer = answer.replace("sqrt", "√")
-                print(answer)
                 text = self.ui.lineEditDisplay.toPlainText()
 
                 self.ui.lineEditDisplay.setText(text+answer)
@@ -391,7 +389,6 @@ class MyForm(QDialog):
             try:
                 answer = json_data["queryresult"]["pods"][1]["subpods"][1]["plaintext"]
                 answer = answer.replace("sqrt", "√")
-                print(answer)
                 text = self.ui.lineEditDisplay.toPlainText()
 
                 self.ui.lineEditDisplay.setText(text+answer)
